effb6ece2f8e261417ff1e8de97fddfea85aae1b
Leandro Lacerda <leandrolacerda.sistema@gmail.com>
2023-06-21 22:03:31 -0300

    Alteracao nas classes Product e MainProduct

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -11,4 +11,26 @@ public class Product {
         this.price = price;
         this.quantity = quantity;
     }
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
 }
+
